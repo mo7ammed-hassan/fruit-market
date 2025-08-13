4aed70bb198d9704ecd2b92351fdf92391d7ac27
Mohamed H. <mohamed7github@gmail.com>
2025-08-13 17:42:56 +0300

    feat(onboarding): enhance skip button visibility and functionality in onboarding flow

--- lib/features/onboarding/presentation/widgets/onboarding_skip.dart
+++ lib/features/onboarding/presentation/widgets/onboarding_skip.dart
@@ -9,28 +9,42 @@ class OnboardingSkip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final cubit = context.read<OnboardingCubit>();
+
     return Padding(
       padding: const EdgeInsets.symmetric(
         horizontal: AppSizes.onbordingPadding,
         vertical: AppSizes.onbordingPadding,
       ),
-      child: Align(
-        alignment: Alignment.topRight,
-        child: GestureDetector(
-          onTap: () => context.read<OnboardingCubit>().skipPage(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                AppStrings.skip,
-                style: Theme.of(context).textTheme.bodyMedium,
+      child: BlocBuilder<OnboardingCubit, int>(
+        buildWhen: (previous, current) => current != previous,
+        builder: (context, state) {
+          final shouldShowSkip = state < cubit.totalPages - 1;
+
+          return Opacity(
+            opacity: shouldShowSkip ? 1 : 0,
+            child: Align(
+              alignment: Alignment.topRight,
+              child: GestureDetector(
+                onTap: !shouldShowSkip
+                    ? null
+                    : () => context.read<OnboardingCubit>().skipPage(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      AppStrings.skip,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                    const SizedBox(height: 4),
+                    Container(height: 1.2, color: Colors.blueGrey, width: 33),
+                  ],
+                ),
               ),
-              const SizedBox(height: 4),
-              Container(height: 1.2, color: Colors.blueGrey, width: 33),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
