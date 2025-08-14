d8491c7e41bb816da5127958fe81163c7fad141f
Mohamed H. <mohamed7github@gmail.com>
2025-08-15 01:46:51 +0300

    feat(auth): navigate to SignUp screen from AuthSwitchText action
    - Updated AuthSwitchData in login screen to open the SignUp screen on action tap.
    - Ensured navigation uses pushReplacement for smoother flow.
    - Keeps navigation consistent with other auth routes.

---
+++ lib/features/auth/presentation/screens/signup_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_market/common/templates/auth_template.dart';
+import 'package:fruit_market/common/widgets/auth_elevated_button.dart';
+import 'package:fruit_market/common/widgets/custom_text_form_field.dart';
+import 'package:fruit_market/common/widgets/phone_number_field.dart';
+import 'package:fruit_market/config/routing/routes.dart';
+import 'package:fruit_market/core/constants/app_sizes.dart';
+import 'package:fruit_market/core/constants/app_strings.dart';
+import 'package:fruit_market/core/models/auth_switch_data.dart';
+import 'package:fruit_market/core/utils/device/extensions/navigation_extension.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AuthTemplate(
+      title: AppStrings.signup,
+      backSceenRoute: Routes.loginScreen,
+      content: Column(
+        children: [
+          const CustomTextFormField(
+            title: AppStrings.fullName,
+            hintText: AppStrings.flName,
+            contentPadding: EdgeInsets.symmetric(
+              vertical: 8.0,
+              horizontal: 18.0,
+            ),
+          ),
+          const SizedBox(height: AppSizes.authSpaceItem * 1.5),
+
+          const PhoneInputWithAutoCountry(title: AppStrings.mobileNumber),
+          const SizedBox(height: AppSizes.authSpaceItem),
+
+          const CustomTextFormField(
+            title: AppStrings.password,
+            hintText: AppStrings.password,
+          ),
+          const SizedBox(height: AppSizes.authSpaceItem * 2),
+
+          AuthElevatedButton(text: AppStrings.signup, onTap: () {}),
+        ],
+      ),
+
+      switchData: AuthSwitchData(
+        title: AppStrings.dontHaveAccount,
+        actionText: AppStrings.login,
+        onActionTap: () => context.pushReplacement(Routes.loginScreen),
+      ),
+
+      spaceAfterContent: 0.04,
+    );
+  }
+}
